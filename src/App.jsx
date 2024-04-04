815834a512a1299a1c5fbec490ed109f7894fc19
Hardin <hardisonpaulino@gmail.com>
2024-04-04 19:15:35 -0400

    first commit; Button component created

---
+++ src/App.jsx
@@ -0,0 +1,70 @@
+// import { useState } from "react";
+import "./App.css";
+import "./styles/testimonial.css";
+
+const Button = (props) => {
+  // console.log(props);
+  return (
+    <>
+      <button className={`btn ${props.className}`}>{props.children}</button>
+    </>
+  );
+};
+
+function drawNumber() {
+  const $screen = document.getElementById("main");
+  document.addEventListener("click", (e) => {
+    // console.log(e.target);
+    console.log(e.target.textContent);
+    console.log($screen);
+    // $screen.value = e.target.textContent;
+  });
+}
+
+function App() {
+  return (
+    <>
+      <h1>Vite + React Calculator</h1>
+      <div className="card">
+        <div className="container">
+          <div className="screen">
+            <input type="number" dir="rtl" name="" id="main" />
+            <input type="number" name="" id="" disabled />
+          </div>
+          <div className="row">
+            <Button className="clean">C</Button>
+            <Button className="function">()</Button>
+            <Button className="function">%</Button>
+            <Button className="function">÷</Button>
+          </div>
+          <div className="row">
+            <Button onClick={drawNumber()}>7</Button>
+            <Button>8</Button>
+            <Button>9</Button>
+            <Button className="function">×</Button>
+          </div>
+          <div className="row">
+            <Button>4</Button>
+            <Button>5</Button>
+            <Button>6</Button>
+            <Button className="function">-</Button>
+          </div>
+          <div className="row">
+            <Button>1</Button>
+            <Button>2</Button>
+            <Button>3</Button>
+            <Button className="function">+</Button>
+          </div>
+          <div className="row">
+            <Button>+/-</Button>
+            <Button>0</Button>
+            <Button>.</Button>
+            <Button className="eq-funct">=</Button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
